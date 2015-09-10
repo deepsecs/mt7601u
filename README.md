6ff5903747b8c8260516b7317592be2b4fce8bff
jiangrs <jiangrs@s-ec.com>
2015-09-10 16:08:13 +0800

    1、完善 README 内容，增加介绍及安装使用说明 2、启动脚本增加互联网接口选择功能

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mt7601u
+
+ubuntu 15.04 平台下 mt7601 驱动/AP工具 及 linux 4.1.6 内核升级包
+
+使用方法:
+1、安装
+sudo bin/miwifi_install.sh
+
+2、卸载
+sudo miwifi_uninstall.sh
+
+3、启动 AP
+sudo miwifi_work.sh
+
--- bin/miwifi_work.sh
+++ bin/miwifi_work.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 #remove the driver before
-ifconfig ra0 down
-rmmod mt7601Usta
+ifconfig ra0 down 2>/dev/null
+rmmod mt7601Usta 2>/dev/null
+
+TITLE="DeepSecs MiWiFi"
+wan=eth0
+
+interfaces=`ifconfig | grep 'Link encap' | grep -v lo | awk '{print $1" "$5}')`
+tmp="$(mktemp)"
+whiptail --menu "互联网接口" --title "${TITLE}" 12 40 0 ${interfaces} >&1 1>&2 2>${tmp}
+rc=$?
+if [ ${rc} -eq 0 ]; then
+	wan="$(cat $tmp)"
+fi
+unlink ${tmp}
+
 #add new ap driver
 modprobe rtutil7601Uap
 modprobe mt7601Uap
@@ -13,9 +26,9 @@ ifconfig ra0 $ip
 #dhcp the network
 dhcpd ra0
 #make if forward work from eth0
-echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+echo 1 | tee /proc/sys/net/ipv4/ip_forward
 iptables -t filter -F
 iptables -t nat -F
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o ${wan} -j MASQUERADE
 echo "Notice: 需要开启 DHCP 服务，否则客户端需要设置成静态获取 IP 地址"
 echo "网关地址: $ip"
