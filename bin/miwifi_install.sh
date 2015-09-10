e9e136172bd50b0813f517d3dc1e56fd85d0589a
jiangrs <jiangrs@s-ec.com>
2015-09-10 11:55:09 +0800

    增加 SSID 和 密码配置交互界面

--- bin/miwifi_install.sh
+++ bin/miwifi_install.sh
@@ -17,4 +17,18 @@ done
 # install configuration files
 mkdir -p /etc/Wireless/RT2870AP
 cp ${ROOT_DIR}/etc/Wireless/RT2870AP/RT2870AP.dat /etc/Wireless/RT2870AP/
+APCONFIG="/etc/Wireless/RT2870AP/RT2870AP.dat"
 
+# setup ssid & wpa/psk password
+tmp="$(mktemp)"
+whiptail --inputbox "请输入 SSID:" 7 30 --title "DeepSecs MiWiFi" "MiWiFi_DeepSecs" >&1 1>&2 2>${tmp}
+ssid="$(cat $tmp)"
+sed -i "s|^SSID=.*|SSID=${ssid}|" ${APCONFIG}
+
+# password
+whiptail --passwordbox "请输入上网密码:" 7 30 --title "DeepSecs MiWiFi" "deepsecs" >&1 1>&2 2>${tmp}
+password="$(cat $tmp)"
+sed -i "s|^WPAPSK=.*|WPAPSK=${password}|" ${APCONFIG}
+
+# delete temp file
+unlink $tmp
